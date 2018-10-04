c3de0eee9d107d49d94b813c6ece63344549bd6d
Alashaq <40842773+Alashaq@users.noreply.github.com>
2018-10-04 13:04:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '$'
 client.on('ready', () => {
   console.log('======================================')
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,74 +10,11 @@ client.on('ready', () => {
   console.log('=======================================')
 });
 
+client.on('message',function(message) {
+    
+    if(message.content.startsWith("<@481969532509093898>")) {
+        message.channel.send('مرحبا انا البنك المركزي للسيد سلطان ')
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "say") {
-let rank = message.guild.member(message.author).roles.find('name', '.');
-if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
-
-
-
-client.on('message', message => {
-    if(message.content === '^^DailyForAll'){
-        message.channel.send('#daily')
     }
 });
-
-client.on('message', message => {
-    if(message.content === '^^AllCredit'){
-        message.channel.send('#credits')
-    }
-});
-
-
-client.on('message', message => {
-    if(message.content === '^^RepForRobben'){
-        message.channel.send('#rep <@371060496276783104>')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '^^RepForBoss'){
-        message.channel.send('#rep <@443152649899212810>')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '^^RepForAlashaq'){
-        message.channel.send('#rep <@346045919072092161>')
-    }
-});
-
-
-
-client.on('message', message => {
-if (message.content === "سبام" + "<@" + `${client.user.id}` + ">") {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-client.login('"MzQ2MDQ1OTE5MDcyMDkyMTYx.DpVjVA.3Y1zGLMwADCq9C2Xqa4DHtOTg3Q"');
+client.login(process.env.TOKEN);
